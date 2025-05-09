ca0ea1491d4e7cddbf26c6a9a36b7f5c68e818e4
Vanni Gallo <vanni.gallo@sky.uk>
2025-05-09 13:20:09 +0100

    remove launch param

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public class Main {
      * The main method that will launch the project.
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main() {
         
         new MainUI();
     }
