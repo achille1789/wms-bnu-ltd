a650e182b5d531785972c4b55555bf33a5dc88dd
Vanni Gallo <vanni.gallo@sky.uk>
2025-09-28 22:02:39 +0100

    update gpt version

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'WMS BNU Ltd. Documentation',
-  tagline: 'Powered by Docusaurus and GPT-5',
+  tagline: 'Powered by Docusaurus and GPT-4o',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
